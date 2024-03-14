a0e593ba3786cb299e9732e6c47891d5acdd6533
FluzyTeck <147531758+fluzyteck@users.noreply.github.com>
2024-03-14 17:14:14 +0000

    2.6

--- Settings/Program/Config/Config.py
+++ Settings/Program/Config/Config.py
@@ -5,6 +5,6 @@ language_tool = "EN"
 creator = "FluzyTeck"
 discord_server = "discord.gg/eWNQ46jqdC"
 website = "red-tiger.000webhostapp.com"
-github_tool = "github.com/fluzyteck/RedTiger"
+github_tool = "github.com/fluzyteck/RedTiger-tools"
 url_config = "https://raw.githubusercontent.com/fluzyteck/RedTiger/main/Settings/Program/Config/Config.py"
 
