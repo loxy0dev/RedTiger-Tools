43acfa0c086dab43a69364b713ea4d951dca94a2
noire <168380676+trulyheartless@users.noreply.github.com>
2024-12-05 13:45:59 -0600

    Update Website-Info-Scanner.py, changing colors

--- Settings/Program/Website-Info-Scanner.py
+++ Settings/Program/Website-Info-Scanner.py
@@ -36,13 +36,13 @@ try:
             website_url = "https://" + url
         else:
             website_url = url
-        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Website: {white}{website_url}{red}")
+        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Website: {white}{website_url}{blue}")
         return website_url
 
     def website_domain(website_url):
         parsed_url = urlparse(website_url)
         domain = parsed_url.netloc
-        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Domain: {white}{domain}{red}")
+        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Domain: {white}{domain}{blue}")
         return domain
 
     def website_ip(domain):
@@ -52,7 +52,7 @@ try:
             ip = "None"
 
         if ip != "None":
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} IP: {white}{ip}{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} IP: {white}{ip}{blue}")
         return ip
 
     def ip_type(ip):
@@ -64,11 +64,11 @@ try:
             type = "Unknown"
             return
         
-        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} IP Type: {white}{type}{red}")
+        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} IP Type: {white}{type}{blue}")
 
     def website_secure(website_url):
         secure = website_url.startswith("https://")
-        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Secure: {white}{secure}{red}")
+        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Secure: {white}{secure}{blue}")
 
     def website_status(website_url):
         try:
@@ -76,7 +76,7 @@ try:
             status_code = response.status_code
         except RequestException:
             status_code = 404
-        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Status Code: {white}{status_code}{red}")
+        print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Status Code: {white}{status_code}{blue}")
 
     def ip_info(ip):
         api_url = f"https://ipinfo.io/{ip}/json"
@@ -88,23 +88,23 @@ try:
 
         host_country = api.get('country', 'None')
         if host_country != "None":
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Country: {white}{host_country}{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Country: {white}{host_country}{blue}")
 
         host_name = api.get('hostname', 'None')
         if host_name != "None":
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Name: {white}{host_name}{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Name: {white}{host_name}{blue}")
 
         host_isp = api.get('isp', 'None')
         if host_isp != "None":
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Isp: {white}{host_isp}{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Isp: {white}{host_isp}{blue}")
 
         host_org = api.get('org', 'None')
         if host_org != "None":
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Org: {white}{host_org}{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host Org: {white}{host_org}{blue}")
 
         host_as = api.get('asn', 'None')
         if host_as != "None":
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host AS: {white}{host_as}{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host AS: {white}{host_as}{blue}")
 
     def ip_dns(ip):
         try:
@@ -113,7 +113,7 @@ try:
             dns = "None"
         
         if dns != "None":
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host DNS: {white}{dns}{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Host DNS: {white}{dns}{blue}")
 
     def website_port(ip):
         port_protocol_map = {
@@ -132,7 +132,7 @@ try:
                 result = sock.connect_ex((ip, port))
                 if result == 0:
                     protocol = identify_protocol(ip, port)
-                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Port: {white}{port}{red} Status: {white}Open{red} Protocol: {white}{protocol}{red}")
+                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Port: {white}{port}{blue} Status: {white}Open{blue} Protocol: {white}{protocol}{blue}")
                 sock.close()
             except:
                 pass
@@ -174,7 +174,7 @@ try:
             response = requests.get(website_url, timeout=5)
             headers = response.headers
             for header, value in headers.items():
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} HTTP Header: {white}{header}{red} Value: {white}{value}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} HTTP Header: {white}{header}{blue} Value: {white}{value}{blue}")
         except RequestException:
             pass
 
@@ -186,7 +186,7 @@ try:
                 sock.connect((urlparse(website_url).hostname, 443))
                 cert = sock.getpeercert()
             for key, value in cert.items():
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} SSL Certificate Key: {white}{key}{red} Value: {white}{value}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} SSL Certificate Key: {white}{key}{blue} Value: {white}{value}{blue}")
         except:
             pass
 
@@ -197,9 +197,9 @@ try:
             headers = response.headers
             for header in headers_of_interest:
                 if header in headers:
-                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Security Header: {white}{header}{red} Value: {white}{headers[header]}{red}")
+                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Security Header: {white}{header}{blue} Value: {white}{headers[header]}{blue}")
                 else:
-                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Missing Security Header: {white}{header}{red}")
+                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Missing Security Header: {white}{header}{blue}")
         except RequestException:
             pass
 
@@ -210,7 +210,7 @@ try:
             for cookie in cookies:
                 secure = 'Secure' if cookie.secure else 'Not Secure'
                 httponly = 'HttpOnly' if cookie.has_nonstandard_attr('HttpOnly') else 'Not HttpOnly'
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Cookie: {white}{cookie.name}{red} Secure: {white}{secure}{red} HttpOnly: {white}{httponly}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Cookie: {white}{cookie.name}{blue} Secure: {white}{secure}{blue} HttpOnly: {white}{httponly}{blue}")
         except RequestException:
             pass
 
@@ -219,8 +219,8 @@ try:
             response = requests.get(website_url, timeout=5, allow_redirects=True)
             if response.history:
                 for resp in response.history:
-                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Redirections: {white}{resp.url}{red} Status: {white}{resp.status_code}{red}")
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Redirections: {white}{response.url}{red} Status: {white}{response.status_code}{red}")
+                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Redirections: {white}{resp.url}{blue} Status: {white}{resp.status_code}{blue}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Redirections: {white}{response.url}{blue} Status: {white}{response.status_code}{blue}")
             else: pass
         except RequestException:
             pass
@@ -232,9 +232,9 @@ try:
             additional_headers = ['Server', 'X-Powered-By', 'X-UA-Compatible', 'X-Permitted-Cross-Domain-Policies']
             for header in additional_headers:
                 if header in headers:
-                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Additional Header: {white}{header}{red} Value: {white}{headers[header]}{red}")
+                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Additional Header: {white}{header}{blue} Value: {white}{headers[header]}{blue}")
                 else:
-                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Missing Additional Header: {white}{header}{red}")
+                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Missing Additional Header: {white}{header}{blue}")
         except RequestException:
             pass
 
@@ -244,8 +244,8 @@ try:
             response = requests.get(website_url, timeout=10)
             end_time = time.time()
             response_time = end_time - start_time
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Response Time: {white}{response_time:.2f} seconds{red}")
-            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Response Size: {white}{len(response.content)} bytes{red}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Response Time: {white}{response_time:.2f} seconds{blue}")
+            print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Response Size: {white}{len(response.content)} bytes{blue}")
         except RequestException:
             pass
 
@@ -255,7 +255,7 @@ try:
             content = response.text
             title_match = re.search(r'<title>(.*?)</title>', content, re.IGNORECASE)
             if title_match:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Page Title: {white}{title_match.group(1)}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Page Title: {white}{title_match.group(1)}{blue}")
             else: pass
         except RequestException:
             pass
@@ -264,19 +264,19 @@ try:
         try:
             result = dns.resolver.resolve(domain, 'A')
             for ipval in result:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS A Record: {white}{ipval.to_text()}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS A Record: {white}{ipval.to_text()}{blue}")
 
             result = dns.resolver.resolve(domain, 'MX')
             for exdata in result:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS MX Record: {white}{exdata.exchange}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS MX Record: {white}{exdata.exchange}{blue}")
 
             result = dns.resolver.resolve(domain, 'TXT')
             for txtdata in result:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS TXT Record: {white}{txtdata.to_text()}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS TXT Record: {white}{txtdata.to_text()}{blue}")
 
             result = dns.resolver.resolve(domain, 'NS')
             for nsdata in result:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS NS Record: {white}{nsdata.to_text()}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} DNS NS Record: {white}{nsdata.to_text()}{blue}")
         except: pass
 
     def detect_technologies(website_url):
@@ -301,9 +301,9 @@ try:
 
             if technologies:
                 for tech in technologies:
-                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Detected Technology: {white}{tech}{red}")
+                    print(f"{BEFORE + current_time_hour() + AFTER} {ADD} Detected Technology: {white}{tech}{blue}")
             else:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} No specific technologies detected{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} No specific technologies detected{blue}")
         except: pass
 
     def analyze_whois(domain):
@@ -311,19 +311,19 @@ try:
             whois_info = whois.whois(domain)
             registrar = whois_info.registrar
             if registrar != None:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Registrar: {white}{registrar}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Registrar: {white}{registrar}{blue}")
 
             creation_date = whois_info.creation_date
             if creation_date != None:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Creation Date: {white}{creation_date}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Creation Date: {white}{creation_date}{blue}")
             
             expiration_date = whois_info.expiration_date
             if expiration_date != None:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Expiration Date: {white}{expiration_date}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Expiration Date: {white}{expiration_date}{blue}")
 
             name_servers = whois_info.name_servers
             if name_servers != None:
-                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Name Servers: {white}{', '.join(name_servers)}{red}")
+                print(f"{BEFORE + current_time_hour() + AFTER} {ADD} WHOIS Name Servers: {white}{', '.join(name_servers)}{blue}")
         except: pass
 
     Slow(scan_banner)
