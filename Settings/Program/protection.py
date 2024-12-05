b7bda9c07eab0566faf6d1376e0f7027cb7cf0c1
noire <168380676+trulyheartless@users.noreply.github.com>
2024-12-05 13:29:39 -0600

    Update protection.py

--- Settings/Program/protection.py
+++ Settings/Program/protection.py
@@ -1,15 +1,18 @@
-
+# /Settings/Program/protection.py
 
 import subprocess
 
 def protection_tool():
     print("Disabling tracking and microphone access...")
-    
     try:
         subprocess.run(['powershell', '-Command', 'Set-ItemProperty -Path "HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\CapabilityAccessManager\\ConsentStore\\microphone" -Name "State" -Value 0 -Force'], check=True)
         print("Microphone access disabled.")
     except Exception as e:
         print(f"Error disabling microphone: {e}")
 
-if __name__ == "__main__":
+def main():
+    input("Press Enter to disable tracking and microphone access...")
     protection_tool()
+
+if __name__ == "__main__":
+    main()
