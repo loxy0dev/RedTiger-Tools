bddb8b1a57c3221f60bcde1734c95cc743f89741
noire <168380676+trulyheartless@users.noreply.github.com>
2024-12-06 11:13:47 -0600

    Update Config.py

--- Settings/Program/Config/Config.py
+++ Settings/Program/Config/Config.py
@@ -1,25 +1,12 @@
-# Copyright (c) RedTiger (https://redtiger.shop)
-# See the file 'LICENSE' for copying permission
-# ----------------------------------------------------------------------------------------------------------------------------------------------------------|
-# EN: 
-#     - Do not touch or modify the code below. If there is an error, please contact the owner, but under no circumstances should you touch the code.
-#     - Do not resell this tool, do not credit it to yours.
-# FR: 
-#     - Ne pas toucher ni modifier le code ci-dessous. En cas d'erreur, veuillez contacter le propriétaire, mais en aucun cas vous ne devez toucher au code.
-#     - Ne revendez pas ce tool, ne le créditez pas au vôtre.
-
-name_tool       =  "RedTiger"
+name_tool       =  "BlueTiger"
 type_tool       =  "Multi-Tools"
-version_tool    =  "6.1"
+version_tool    =  "0.3"
 coding_tool     =  "Python 3"
 language_tool   =  "EN"
-creator         =  "loxy0dev"
+creator         =  "lossofclarity"
 platform        =  "Windows 10/11 & Linux"
 discord_server  =  "None"
-website         =  "redtiger.shop"
-github_tool     =  "github.com/loxy0dev/RedTiger-Tools"
-telegram        =  "t.me/redtigersupport"
-url_config      =  "https://raw.githubusercontent.com/loxy0dev/RedTiger-Tools/main/Settings/Program/Config/Config.py"
-url_downloads   =  "https://github.com/loxy0dev/RedTiger-Tools/archive/main.zip"
-license         =  "https://github.com/loxy0dev/RedTiger-Tools/blob/main/LICENSE"
-copyright       =  "Copyright (c) RedTiger 'GPL-3.0 license'"
+website         =  "sachi.lol"
+github_tool     =  "https://github.com/trulyheartless/BlueTiger"
+telegram        =  "t.me/fatebreaker"
+url_config      =  "https://raw.githubusercontent.com/trulyheartless/BlueTiger/main/Settings/Program/Config/Config.py"
